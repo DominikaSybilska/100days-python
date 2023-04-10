d916a72d79fd0e40dbc58bf5d160c62a39299293
d000mka <d.sybilska@wp.pl>
2023-04-10 21:52:40 +0200

    day 17 commit

---
+++ day17/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+if __name__ == '__main__':
+    question_bank = []
+    for question in question_data:
+        question_bank.append(Question(question["question"], question["correct_answer"]))
+
+    quiz = QuizBrain(question_bank)
+    while quiz.still_has_question():
+        quiz.next_question()
+    print("You've competed the quiz")
+    print(f"Your final score was: {quiz.score}/{len(question_bank)}")
